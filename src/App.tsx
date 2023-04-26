6b1d4990688268ae3661678d6f85ce6d2d73d8ef
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2023-04-26 22:26:51 +0900

    various changes added react router fixed main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import Login from './pages/login';
+import React, { useEffect, useLayoutEffect } from 'react';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import RootLayout from './components/RootLayout';
+import { CssVarsProvider } from '@mui/joy/styles/CssVarsProvider';
+import customTheme from './theme';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+
+const useEnhancedEffect =
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
 function App() {
-  return <Login />;
+  return (
+    // <Login />
+    <CssVarsProvider disableTransitionOnChange theme={customTheme}>
+      <GlobalStyles
+        styles={{
+          '[data-feather], .feather': {
+            color: 'var(--Icon-color)',
+            margin: 'var(--Icon-margin)',
+            fontSize: 'var(--Icon-fontSize, 20px)',
+            width: '1em',
+            height: '1em',
+          },
+        }}
+      />
+      <RootLayout>
+        <Dashboard />
+      </RootLayout>
+    </CssVarsProvider>
+  );
 }
 
 export default App;
---
+++ src/components/RootLayout.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Box from '@mui/joy/Box';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import MainNavigation from './MainNavigation';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography/Typography';
+import ColorSchemeToggle from './utils/ColorSchemeToggle';
+
+export default function RootLayout({ children }) {
+  return (
+    <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+      <MainNavigation />
+      <Box
+        component="main"
+        className="MainContent"
+        sx={(theme) => ({
+          px: {
+            xs: 2,
+            md: 6,
+          },
+          pt: {
+            xs: `calc(${theme.spacing(2)} + var(--Header-height))`,
+            sm: `calc(${theme.spacing(2)} + var(--Header-height))`,
+            md: 3,
+          },
+          pb: {
+            xs: 2,
+            sm: 2,
+            md: 3,
+          },
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          minWidth: 0,
+          height: '100dvh',
+          gap: 1,
+        })}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Breadcrumbs
+            size="sm"
+            aria-label="breadcrumbs"
+            separator={<i data-feather="chevron-right" />}
+            sx={{
+              '--Breadcrumbs-gap': '1rem',
+              '--Icon-fontSize': '16px',
+              fontWeight: 'lg',
+              color: 'neutral.400',
+              px: 0,
+            }}
+          >
+            <Link
+              underline="none"
+              color="neutral"
+              fontSize="inherit"
+              href="#some-link"
+              aria-label="Home"
+            >
+              <i data-feather="home" />
+            </Link>
+            <Link
+              underline="hover"
+              color="neutral"
+              fontSize="inherit"
+              href="#some-link"
+            >
+              Dashboard
+            </Link>
+            <Typography fontSize="inherit" variant="soft" color="primary">
+              Orders
+            </Typography>
+          </Breadcrumbs>
+          <ColorSchemeToggle
+            sx={{ ml: 'auto', display: { xs: 'none', md: 'inline-flex' } }}
+          />
+        </Box>
+        <Box>{children}</Box>
+      </Box>
+    </Box>
+  );
+}
